79eec29d497c3686346f2629bd74534a1dfbdbad
glassruler <151485064+glassruler@users.noreply.github.com>
2023-11-22 14:28:32 +0700

    Update collab.py

--- collab.py
+++ collab.py
@@ -121,7 +121,7 @@ else:
 category_df = filtered_df.groupby(by = ["Category"], as_index = False)["Sales"].sum()
 
 
-cl1, cl2 = st.columns((2))
+cl1, cl2, cl3 = st.columns((3))
 with cl1:
     with st.expander("Category_ViewData"):
         st.write(category_df)
@@ -135,6 +135,14 @@ with cl2:
         st.write(region)
         csv = region.to_csv(index = False).encode('utf-8')
         st.download_button("Download Data", data = csv, file_name = "Cabang.csv", mime = "text/csv",
+                        help = 'Click here to download the data as a CSV file')
+
+with cl3:
+    with st.expander("Gametitle_ViewData"):
+        region = filtered_df.groupby(by = "GameTitle", as_index = False)["Sales"].sum()
+        st.write(region)
+        csv = region.to_csv(index = False).encode('utf-8')
+        st.download_button("Download Data", data = csv, file_name = "Gametitle.csv", mime = "text/csv",
                         help = 'Click here to download the data as a CSV file')
 
 filtered_df["month_year"] = filtered_df["Order Date"].dt.to_period("M")
