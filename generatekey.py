3d2620a9597c5d07d602354d85137884aaa66c82
glassruler <151485064+glassruler@users.noreply.github.com>
2023-12-13 11:37:55 +0700

    Update generatekey.py

--- generatekey.py
+++ generatekey.py
@@ -5,7 +5,7 @@ import streamlit_authenticator as stauth
 
 names = ["Wahyu Hidayat", "Admin Zobu","Roger Tumewu"]
 usernames = ["hidayat", "admzobu", "roger"]
-passwords = ["xxx", "xxx", "xxx"]
+paswords = ["123456", "12345678", "zone2000"]
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
