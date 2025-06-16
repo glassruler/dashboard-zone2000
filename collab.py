e8ce1a35708641ea9f4cfaa1bfd16fd938979161
glassruler <151485064+glassruler@users.noreply.github.com>
2025-06-16 16:43:26 +0700

    Update collab.py select location month

--- collab.py
+++ collab.py
@@ -31,13 +31,13 @@ elif authentication_status:
     # Load Data
     @st.cache_data(ttl=600)  # Refresh cache every 10 minutes
     def load_data():
-        dict_df = pd.read_excel('collabreport.xlsx', sheet_name=['dataomzet', 'dataomzet___', 'datagame', 'datach'])
+        dict_df = pd.read_excel('collabreport.xlsx', sheet_name=['dataomzet', 'dataomzet__', 'datagame', 'datach'])
         
         dict_df['dataomzet']["BulanTahun"] = pd.to_datetime(dict_df['dataomzet']["BulanTahun"], errors='coerce')
         dict_df['dataomzet']["month_year"] = dict_df['dataomzet']["BulanTahun"].dt.to_period("M")
         
-        dict_df['dataomzet___']["Bulan"] = pd.to_datetime(dict_df['dataomzet___']["Bulan"], format='%m')
-        dict_df['dataomzet___']["month_year"] = dict_df['dataomzet___']["Bulan"].dt.to_period("M")
+        dict_df['dataomzet__']["Bulan"] = pd.to_datetime(dict_df['dataomzet__']["Bulan"], format='%m')
+        dict_df['dataomzet__']["month_year"] = dict_df['dataomzet__']["Bulan"].dt.to_period("M")
         
         dict_df['datagame']["Order Date"] = pd.to_datetime(dict_df['datagame']["Order Date"], errors='coerce')
         
@@ -46,7 +46,7 @@ elif authentication_status:
 
     dict_df = load_data()
     dataomzet_df = dict_df['dataomzet']
-    dataomzet_new_df = dict_df['dataomzet___']  # New sheet with sales data for comparison
+    dataomzet_new_df = dict_df['dataomzet__']  # New sheet with sales data for comparison
     datagame_df = dict_df['datagame']
 
     # Sidebar for user filters and logout
